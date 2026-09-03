cc00edf28ef83cdd106c861f04fdbea67572949d
AshutoshBasnet <romenrengs102@gmail.com>
2026-09-03 09:33:45 +0545

    ok

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Connect to backend via proxy in dev or current origin in prod
-export const socket = io({
+export const socket = io(import.meta.env.VITE_API_BASE_URL || "https://api.ashutoshcodes.me/socket.io", {
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: 10,
